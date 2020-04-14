00008f3fb0a88af4f9a8777d41f92b5c10cc5d07
shravani desai <44225921+shravani1510desai@users.noreply.github.com>
2020-04-14 19:37:21 +0530

    Add files via upload

---
+++ covid.R
@@ -0,0 +1,9 @@
+?data.frame
+covid1 <- read.csv(file=file.choose())
+file.choose()
+covid2 <-read.table("D:\\Shravani\\kaggel\\covid_19_data.csv")
+summary(covid1)
+colnames(covid1)
+dim(covid1)
+
+
